7e08ed4218e7451d960cb0d2312ab622dd216838
amit kumar <akumar@deqode.com>
2022-01-07 18:31:49 +0530

    add some test cases

--- RANDOMTASK/math.js
+++ RANDOMTASK/math.js
@@ -1,8 +1,20 @@
-const calculateTip = (total, tipPercent) => {
+const calculateTip = (total, tipPercent = 0.25) => {
   const tip = total * tipPercent;
   return total + tip;
 };
 
+const fahrenheitToCelsius = (temp) => {
+  const result = (temp - 32) / 1.8;
+  return result;
+};
+
+const celsiusToFahrenheit = (temp) => {
+  const result = (temp * 1.8) + 32;
+  return result;
+};
+
 module.exports = {
   calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
 };
--- Tests07_01_22/math.test.js
+++ Tests07_01_22/math.test.js
@@ -1,11 +1,27 @@
 /* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
-const { calculateTip } = require('../RANDOMTASK/math');
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../RANDOMTASK/math');
 
 test('should calculate total with tip', () => {
   const total = calculateTip(10, 0.3);
   expect(total).toBe(13);
 });
+
+test('should calculate total with default tip', () => {
+  const total = calculateTip(10);
+  expect(total).toBe(12.5);
+});
+
+test('should convert firenheit to celcius', () => {
+  const temp = fahrenheitToCelsius(32);
+  expect(temp).toBe(0);
+});
+
+test('should convert celcius to farenheit', () => {
+  const temp = celsiusToFahrenheit(0);
+  expect(temp).toBe(32);
+});
+
 // test('firstTest', () => {
 
 // });
